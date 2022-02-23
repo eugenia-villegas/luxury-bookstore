abed9ed4fc01a3b09582cb8da5312d2bd166a24d
Eugenia Villegas <villegasmeuge@gmail.com>
2022-02-23 00:01:10 -0300

    Desafio counter esconder y mostrar cart

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,9 +3,15 @@ import { Input } from "./styledComponents.js";
 import { SearchContainer } from "./styledComponents.js";
 
 const SearchForm = () => {
+    const handleClick = (evt) => {
+        evt.stopPropagation();
+        alert("Hola Input")
+    }
+    
+
     return (
         <SearchContainer>
-            <Input />
+            <Input onClick={handleClick}/>
             <IoSearchOutline />
         </SearchContainer>
     )
